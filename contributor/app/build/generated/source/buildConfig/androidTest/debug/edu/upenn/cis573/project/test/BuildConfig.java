8cc629856476d84c2bcfdd38f2e1d3fd65773139
DENNIS WANG <denniswz123@gmail.com>
2024-12-24 22:45:10 -0500

    update

---
+++ contributor/app/build/generated/source/buildConfig/androidTest/debug/edu/upenn/cis573/project/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.upenn.cis573.project.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.upenn.cis573.project.test";
+  public static final String BUILD_TYPE = "debug";
+}
